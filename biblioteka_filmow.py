8273584349add1b719fb9825f063ce013d98859c
Kamil Laptop <kkw.kaminski@gmail.com>
2023-12-18 22:53:17 +0100

    dodano funkcję generate_views oraz generate_views_multiple_times

--- biblioteka_filmow.py
+++ biblioteka_filmow.py
@@ -1,3 +1,5 @@
+import random
+
 class MediaLibrary:
     def __init__(self):
         self.media_list = []
@@ -18,6 +20,16 @@ class MediaLibrary:
     def search(self, title):
         return [media for media in self.media_list if media.title.lower() == title.lower()]
 
+    def generate_views(self):
+        if self.media_list:
+            selected_media = random.choice(self.media_list)
+            views = random.randint(1, 100)
+            selected_media.play_count += views
+
+    def generate_views_multiple_times(self, times=10):
+        for _ in range(times):
+            self.generate_views()
+
 
 class Movie:
     def __init__(self, title, release_year, genre):
@@ -30,7 +42,7 @@ class Movie:
         self.play_count += 1
 
     def __str__(self):
-        return f"{self.title} ({self.release_year})"
+        return f"{self.title} ({self.release_year}) - {self.play_count} views"
 
 
 class TVShow:
@@ -46,7 +58,7 @@ class TVShow:
         self.play_count += 1
 
     def __str__(self):
-        return f"{self.title} S{self.season:02d}E{self.episode:02d} ({self.release_year})"
+        return f"{self.title} S{self.season:02d}E{self.episode:02d} ({self.release_year}) - {self.play_count} views"
 
 
 if __name__ == "__main__":
@@ -66,22 +78,8 @@ if __name__ == "__main__":
     movie1.play()
     tv_show1.play()
 
+    # Generujemy odtworzenia
+    library.generate_views_multiple_times()
+
     # Wyświetlamy zawartość biblioteki
     library.display_media()
-
-    # Wyświetlamy filmy
-    print("\nFilmy:")
-    for movie in library.get_movies():
-        print(movie)
-
-    # Wyświetlamy seriale
-    print("\nSeriale:")
-    for series in library.get_series():
-        print(series)
-
-    # Wyszukujemy tytuł
-    search_title = "The Simpsons"
-    search_results = library.search(search_title)
-    print(f"\nWyszukiwanie dla '{search_title}':")
-    for result in search_results:
-        print(result)
